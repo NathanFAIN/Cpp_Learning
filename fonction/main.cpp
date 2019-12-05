cd66723f61ae105cd7920df4ba14b9caaf76d4e3
NathanFAIN <nathan.fain@epitech.eu>
2019-12-05 10:37:03 +0100

    push fonction

--- fonction/main.cpp
+++ fonction/main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+template<typename T>
+T sum(T a, T b)
+{
+    return (a + b);
+}
+
 int main(void)
 {
+    int a{0};
+    auto fun = [&](int n){a = n;};
+
+    fun(1);
+    cout << a << endl;
+    cout << sum(1, 1) << endl;
     return (0);
 }
